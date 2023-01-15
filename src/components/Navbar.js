2f50a9f92bff31f75ca94005feb42dfd5ae9379f
Toluhi Kay <toluhikay@users.noreply.github.com>
2023-01-15 20:29:22 +0100

    href

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,19 @@
-import React from "react"
-import { NavLink, Link } from "react-router-dom"
-export default function Navbar (){
-    return (
-        <nav className="navbar-section">
-            <ul className="navbar">
-                <NavLink to="/">
-                    <li>
-                        About
-                    </li>
-                </NavLink>
-                <NavLink to= "/Projects">
-                    <li>
-                        Projects
-                    </li>
-                </NavLink>
-                <Link to="/Projects#contact">
-                    <li>
-                        Contact
-                    </li>
-                </Link>
-                
-            </ul>
-        </nav>
-    )
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+export default function Navbar() {
+  return (
+    <nav className="navbar-section">
+      <ul className="navbar">
+        <NavLink to="/">
+          <li>About</li>
+        </NavLink>
+        <NavLink to="/Projects">
+          <li>Projects</li>
+        </NavLink>
+        <Link href="/Projects#contact" to="/Projects#contact">
+          <li>Contact</li>
+        </Link>
+      </ul>
+    </nav>
+  );
 }
